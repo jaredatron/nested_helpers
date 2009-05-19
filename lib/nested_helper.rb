a4d996e9ce8196ab404a8cf9247cebb176de704c
Jared Grippe <jared@deadlyicon.com>
2009-05-19 16:48:59 -0700

    added partial rendering support renamed conflicting method name "render" to "to_html"

--- lib/nested_helper.rb
+++ lib/nested_helper.rb
@@ -1,4 +1,9 @@
 class NestedHelper
+  
+  def self.new(*)
+    super.to_html
+  end
+  
   def initialize(parent, *args, &block)
     @called = []
     @parent = parent
@@ -7,30 +12,54 @@ class NestedHelper
     init_options(*args)
   end
   attr_reader :parent, :called, :block, :args
-  
-  def method_missing(method, *args, &block)
-    parent.send(method, *args, &block)
-  end
+  attr_accessor :html_options, :partial_options
   
   def respond_to?(method)
     super or parent.respond_to?(method)
   end
   
-  def capture_block
-    self_recorder = SendRecorder.new(self)
-    returning capture(self_recorder, &block) do
-      @called = self_recorder.__called__ #.clone ??
-    end
+  def name
+    self.class.to_s.gsub(/^(?:.*::)?(\w+)$/,'\1')
+  end
+
+  private
+
+  def method_missing(method, *args, &block)
+    parent.send(method, *args, &block)
   end
   
   # overwrite this to handle the options passed to your helper
   def init_options(*args)
     @html_options = args.first || {}
+    @partial_options = html_options.delete(:render)
   end
-  attr_accessor :html_options
+  
+  def capture_block
+    return nil if block.nil?
+    self_recorded = SendRecorder.new(self)
+    returning capture(self_recorded, &block) do
+      @called = self_recorded.__called__ #.clone ??
+    end
+  end
+  
+  def render_partial
+    return nil unless partial_options
+    self_recorded = SendRecorder.new(self)
+    partial_options[:locals] ||= {}
+    partial_options[:locals][name.downcase.to_sym] = self_recorded
+    partial_options[:locals][name.downcase[0,1].to_sym] = self_recorded
+    returning render(partial_options) do
+      @called = self_recorded.__called__ #.clone ??
+    end
+  end
+
+  def captured_content
+    @captured_content ||= capture_block or render_partial
+  end
   
   # overwrite this to customize the rendering of your helper
-  def render
-    capture_block
+  def to_html
+    concat(captured_content)
   end
+
 end
--- lib/object.rb
+++ lib/object.rb
@@ -7,9 +7,9 @@ class Object
     klass_name = name.classify
     const_set(klass_name.to_sym, Class.new(NestedHelper, &block))
     class_eval <<-CODE
-      def #{name}(*args, &block)                       # def purple_box(*args, &block)
-        #{klass_name}.new(self, *args, &block).render  #   PurpleBox.new(self, *args, &block).render
-      end                                              # end
+      def #{name}(*args, &block)               # def purple_box(*args, &block)
+        #{klass_name}.new(self, *args, &block) #   PurpleBox.new(self, *args, &block).render
+      end                                      # end
     CODE
   end
 end
