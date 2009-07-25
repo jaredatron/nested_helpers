2bb61252119c9a9075cc9ec43b36b1e50c3412e2
Jared Grippe <jared@deadlyicon.com>
2009-07-24 21:56:03 -0700

    complete rewrite

--- lib/nested_helper.rb
+++ lib/nested_helper.rb
@@ -1,65 +1,72 @@
+#  Nested Helper
+#
+#
+#     module WizrdHelper
+#       class Wizard < NestedHelper
+#         def step(html_options, &block)
+#           html_options[:class] = "step #{html_options[:class]}".strip
+#           haml_tag(:div, capture(Step.new(self), &block), html_options)
+#         end
+#         
+#         class Step < NestedHelper
+#
+#           def link_to_prev(html_options, &block)
+#             html_options.reverse_merge!(:href => 'javascript:void(null);', :class => 'prev_step')
+#             haml_tag(:a,html_options) do
+#               concat(capture(&block) || 'prev')
+#             end
+#           end
+#
+#           def link_to_next(html_options, &block)
+#             html_options.reverse_merge!(:href => 'javascript:void(null);', :class => 'next_step')
+#             haml_tag(:a,html_options) do
+#               concat(capture(&block) || 'next')
+#             end
+#           end
+#
+#           define_partial_proxy_helper :submit => 'wizard/submit'
+#
+#         end
+#       end
+#     end
+#
+#
+#
+#    - form_for @user do |f|
+#      - wizard :id => "SignupWizard" do |w|
+#        - w.step :class => 'credentials' do
+#          = render :partial => 'static/homepage/signup_form/credentials', :locals => {:f => f}
+#        - w.step :class => 'gamer_info' do |w|
+#          - w.link_to_prev do
+#            back
+#          - w.link_to_next do
+#            Go!
+#          - w.submit
+#        - w.step :class => 'waiting'
+#          %h1
+#            Creating your new Rupture account...
 class NestedHelper
-  
-  def self.new(*)
-    super.to_html
-  end
-  
-  def initialize(parent, *args, &block)
-    @called = []
-    @parent = parent
-    @args = args
-    @block = block
-    init_options(*args)
-  end
-  attr_reader :parent, :called, :block, :args
-  attr_accessor :html_options, :partial_options
-  
-  def respond_to?(method)
-    super or parent.respond_to?(method)
-  end
-  
-  def name
-    self.class.to_s.gsub(/^(?:.*::)?(\w+)$/,'\1')
-  end
 
-  private
-
+  def self.define_partial_proxy_helper(method_to_partial)
+    method = method_to_partial.keys.first
+    partial = method_to_partial.values.first
+    class_eval <<-CODE
+      def #{method.to_sym}(options={}, &block)                        # def red_slipper(*args, &block)
+        body ||= capture(&block)  if block_given?                     # body ||= capture(&block)  if block_given?
+        locals = {:options => options, :body => body}                 # locals = {:options => options, :body => body}
+        concat( render(:partial => '#{partial}', :locals => locals))  # concat( render(:partial => 'shoes/red_slipper', :locals => locals))
+      end                                                             # end
+    CODE
+  end
+  
+  def initialize(action_view)
+    action_view = action_view.instance_variable_get('@action_view')  if action_view.is_a?(NestedHelper)
+    action_view.is_a?(ActionView::Base)  or raise ArgumentError.new("the first argument to a nested helper must be an instance of ActionView::Base not #{action_view.class}")
+    @action_view = action_view
+  end
+  
   def method_missing(method, *args, &block)
-    parent.send(method, *args, &block)
-  end
-  
-  # overwrite this to handle the options passed to your helper
-  def init_options(*args)
-    @html_options = args.first || {}
-    @partial_options = html_options.delete(:render)
-  end
-  
-  def capture_block
-    return nil if block.nil?
-    self_recorded = SendRecorder.new(self)
-    returning capture(self_recorded, &block) do
-      @called = self_recorded.__called__ #.clone ??
-    end
-  end
-  
-  def render_partial
-    return nil unless partial_options
-    self_recorded = SendRecorder.new(self)
-    partial_options[:locals] ||= {}
-    partial_options[:locals][name.downcase.to_sym] = self_recorded
-    partial_options[:locals][name.downcase[0,1].to_sym] = self_recorded
-    returning render(partial_options) do
-      @called = self_recorded.__called__ #.clone ??
-    end
-  end
-
-  def captured_content
-    @captured_content ||= capture_block or render_partial
-  end
-  
-  # overwrite this to customize the rendering of your helper
-  def to_html
-    concat(captured_content)
+    @action_view.send(method, *args, &block)
   end
 
 end
--- lib/nested_helpers.rb
+++ lib/nested_helpers.rb
@@ -1,7 +1,1 @@
-module NestedHelpers
-end
-
-$LOAD_PATH.unshift File.dirname(__FILE__)
-require 'object'
-require 'nested_helper/send_recorder'
-require 'nested_helper'
+require File.join(File.dirname(__FILE__), 'nested_helper')
